b73eb40f858ccc35df34f9534a2f60d539ddd01d
phongamnart <pknart17@gmail.com>
2024-03-04 15:25:14 +0700

    push

--- main.go
+++ main.go
@@ -1,75 +1,47 @@
 package main
 
 import (
-	"database/sql"
     "encoding/json"
-    "fmt"
     "log"
     "net/http"
 
-    _ "github.com/lib/pq"
-)
-
-var db *sql.DB
-
-const (
-    host     = "localhost"
-    port     = 5432
-    user     = "postgres"
-    password = "tamarin17"
-    dbname   = "tukodnam"
+    "gorm.io/driver/postgres"
+    "gorm.io/gorm"
+    "github.com/gorilla/handlers"
 )
 
 type Menu struct {
-	Product 	string `json:"product"`
-	Price		string `json:"price"`
-	Quantity	string `json:"quantity"`
+    ID       uint   `gorm:"primaryKey"`
+    Product  string
+    Price    float64
+    Quantity int
 }
 
-func connectToDB() error {
-	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-	var err error
-	db, err = sql.Open("postgres", connStr)
-	if err != nil {
-		return err
-	}
-	if err = db.Ping(); err != nil {
-		return err
-	}
-	return nil
-}
+var db *gorm.DB
 
-func handleGet (w http.ResponseWriter, r *http.Request) {
-	rows , err := db.Query("SELECT * FROM menu")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer rows.Close()
-	var menu []Menu
-	for rows.Next() {
-		var mn Menu
-		if err := rows.Scan(&mn.Product, &mn.Price, &mn.Quantity); err != nil {
-			log.Println(err)
-			continue
-		}
-		menu = append(menu, mn)
-	}
-	if err := rows.Err(); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	json.NewEncoder(w).Encode(menu)
-}
 
 func main() {
-	if err := connectToDB(); err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
+    var err error
+    dsn := "host=localhost user=postgres password=tamarin17 dbname=tukodnam port=5432 sslmode=disable"
+    db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+    if err != nil {
+        log.Fatal(err)
+    }
+    db.AutoMigrate(&Menu{})
 
-	http.HandleFunc("/menu", handleGet)
+    http.HandleFunc("/menus", getMenus)
 
-	log.Fatal(http.ListenAndServe(":9000", nil))
+    allowedHeaders := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+    allowedOrigins := handlers.AllowedOrigins([]string{"*"})
+    allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"})
+    cors := handlers.CORS(allowedHeaders, allowedOrigins, allowedMethods)
+
+    log.Println("Server started at :9000")
+    log.Fatal(http.ListenAndServe(":9000", cors(http.DefaultServeMux)))
+}
+
+func getMenus(w http.ResponseWriter, r *http.Request) {
+    var menus []Menu
+    db.Find(&menus)
+    json.NewEncoder(w).Encode(menus)
 }
